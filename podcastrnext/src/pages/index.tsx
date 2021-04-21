95f31f625342a3ca931c95f3735d783bb9255c8b
AlysonRM <alysonronnan@gmail.com>
2021-04-21 18:16:16 -0300

    SSR para consumir dados de API

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -2,17 +2,22 @@
 // SSR
 // SSG
 
-import {useState} from 'react'
-
-export default function Home() {
-	useState ( () => {
-    fetch('http://localhost:3333/episodes')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  })
+export default function Home(props) {
   return (
-
-  	<h1>Index</h1>
-
+    <div>
+  	  <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getServerSideProps () {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
 }
