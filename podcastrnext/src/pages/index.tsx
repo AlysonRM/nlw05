752f3cf6f4c27d1e4243cbf12fa5221bb7e352d1
AlysonRM <alysonronnan@gmail.com>
2021-04-21 17:55:24 -0300

    Requisição em SPA

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -2,9 +2,14 @@
 // SSR
 // SSG
 
+import {useState} from 'react'
 
 export default function Home() {
-	
+	useState ( () => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  })
   return (
 
   	<h1>Index</h1>
